1d4c9d22de5fe01929baeeefdf1313b4bac044f9
Fei Yuan <yuan@nscl.msu.edu>
2016-03-05 06:17:34 -0500

    hw5 done

--- hw5/make.py
+++ hw5/make.py
@@ -1,97 +1,35 @@
 from makegen import *
 
 def make_all():
-    return alias("all", [bench()]).merge(
-        simple_command("makegen {0}", "Makefile", ["make.py"], no_clean=True),
-        Ruleset(macros={
-            "CFLAGS": "-Wall -O3",
-            "LIBS": "-lm",
-        }),
-    )
-
-def bench():
-    return simple_command(
-        "cd ../ext/morth && ./morth1",
-        "_dummy",
-        [build()],
+    return alias("all", [build(), report()]).merge(
+        simple_command(
+            "@if command -v makegen >/dev/null 2>&1; then "
+            "printf 'Updating makefile ...\\n'; makegen {0}; else "
+            "touch {0}; fi",
+            "Makefile", ["make.py"], no_clean=True),
+        bench(),
     )
 
 def build():
     return simple_command(
-        "cd ../ext/morth && $(MAKE) COMPILER_FAMILY=gcc CC=gcc LIBS=-lm",
-        "morth1",
+        "cd ../ext/morth && $(MAKE)",
+        "../ext/morth/morth1",
+        [],
     ).merge(Ruleset(clean_cmds=["cd ../ext/morth && $(MAKE) clean"]))
 
-def data():
+def bench():
     return simple_command(
-        "python {0} merge {out} {all1}",
-        "data.json",
-        ["main.py"] + benchs(),
-    )
-
-def analysis():
-    return simple_command(
-        "python {0} analyze {out} {all1}",
-        "analysis.json",
-        ["main.py", data()],
-    )
-
-def plot():
-    return simple_command(
-        "python {0} plot {out} {all1}",
-        "figs.json",
-        ["main.py", analysis()],
+        "{0} $(BENCHFLAGS)",
+        "bench",
+        [build()],
+        phony=True,
     )
 
 def report():
     return simple_command(
         "python {0} report {out} {all1}",
         "index.html",
-        ["main.py", analysis(), plot(), "template.html"],
+        ["main.py", "template.html", "output.txt", "vectorization.txt"],
     )
-
-def prefix(name):
-    import os
-    return "{0}_{1}".format(
-        os.path.basename(os.path.dirname(os.path.abspath(__file__))),
-        name,
-    )
-
-def bench_program(suffix=None, macros={}, extra_flags={}):
-    b = RelocatedBuilder(root="..")
-    build = b.build_program(
-        "{0}{1}".format(prefix(NAME), suffix or ""),
-        [
-            b.compile_source(
-                NAME + ".c",
-                macros=macros,
-                suffix=suffix,
-                extra_flags=extra_flags,
-            ),
-            b.compile_source("../utils/utils.c"),
-            b.compile_source("../utils/time.c"),
-        ],
-        libraries="$(LIBS)",
-    )
-    def bench(buildall):
-        return simple_command(
-            "python main.py bench {out} {0}",
-            "../dist/tmp/{0}{1}.json".format(prefix(NAME), suffix or ""),
-            [build, buildall],
-        )
-    return build, bench
-
-def compile_before_benching(build_bench_list):
-    '''Make sure everything is compiled before starting the benchmarks.'''
-    build_bench_list = tuple(build_bench_list)
-    builds = [build for build, _ in build_bench_list]
-    # .PHONY overrides .SECONDARY so we need an extra indirection
-    buildall = simple_command(
-        "touch {out}",
-        "../dist/tmp/{0}_build.ok".format(prefix(NAME)),
-        builds,
-    )
-    buildall = buildall.merge(alias("build", [buildall]))
-    return [bench(buildall) for _, bench in build_bench_list]
 
 make_all().save()
