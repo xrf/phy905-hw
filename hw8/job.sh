1af4d9fa31f2eec537cca2b2ba1cb0c3336a0076
Fei Yuan <yuan@nscl.msu.edu>
2016-03-29 03:56:33 -0400

    Tweak settings to work on MSU HPC

--- hw8/job.sh
+++ hw8/job.sh
@@ -4,6 +4,6 @@
 #PBS -l walltime=15:00
 #PBS -l mem=256mb
 set -eu
-module add GNU/5.2 Python3/3.3.2
+module add GNU/4.9 OpenMPI/1.10.0
 cd "$HOME/stuff/phy905-hw/hw8"
-make WSIZE="$PBS_NUM_NODES" bench
+make CFLAGS="-Wall -Wextra -O3 -std=c11" WSIZE="$PBS_NUM_NODES" bench
