96a3db0f51d8482af4c9e9f7ed53109b5f65dbc8
Fei Yuan <yuan@nscl.msu.edu>
2016-04-19 06:57:37 -0400

    Submit jobs separately

--- hw9/submit.sh
+++ hw9/submit.sh
@@ -3,8 +3,15 @@
 set -eu
 mkdir -p ../dist/tmp/hw9
 for num_nodes in 8 16 32 64; do
-    # for some reason, qsub with stdin is really buggy, so we have to make a
-    # temporary file instead
-    sed "s/{{num_nodes}}/$num_nodes/g" job.sh.template >../dist/tmp/hw9/job.sh
-    qsub ../dist/tmp/hw9/job.sh
+    for m in 001024 016384 102400; do
+        for method in 1 2; do
+            # for some reason, qsub with stdin is really buggy, so we have to
+            # make a temporary file instead
+            sed -e "s/{{num_nodes}}/$num_nodes/g;
+                    s/{{m}}/$m/g;
+                    s/{{method}}/$method/g" <job.sh.template \
+                >../dist/tmp/hw9/job.sh
+            qsub ../dist/tmp/hw9/job.sh
+        done
+    done
 done
