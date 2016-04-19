2401ac6a606553e4f1f449a33d6ab18fed0f6230
Fei Yuan <yuan@nscl.msu.edu>
2016-04-19 06:09:20 -0400

    Use tmp file for qsub due to bug

--- hw9/submit.sh
+++ hw9/submit.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # Note: this script is tailored to Blue Waters
 set -eu
+mkdir -p ../dist/tmp/hw9
 for num_nodes in 8 16 32 64; do
-    sed "s/{{num_nodes}}/$num_nodes/g" job.sh.template | qsub
+    # for some reason, qsub with stdin is really buggy, so we have to make a
+    # temporary file instead
+    sed "s/{{num_nodes}}/$num_nodes/g" job.sh.template >../dist/tmp/hw9/job.sh
+    qsub ../dist/tmp/hw9/job.sh
 done
